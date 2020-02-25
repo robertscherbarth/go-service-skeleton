57651b9c41312b4d511e8e7e5d8c7df3ad95e668
Robert Scherbarth <r.scherbarth@icloud.com>
2020-02-25 09:29:31 +0100

    add logrus as standard logger

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/robertscherbarth/go-service-skeleton
 
 go 1.13
 
-require github.com/prometheus/client_golang v1.3.0
+require (
+	github.com/prometheus/client_golang v1.3.0
+	github.com/sirupsen/logrus v1.4.2
+)
--- main.go
+++ main.go
@@ -2,24 +2,34 @@ package main
 
 import (
 	"io"
-	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	log "github.com/sirupsen/logrus"
 )
 
+const port = 8080
+
 func main() {
-	log.Println("Starting service ...")
+
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp: true,
+	})
+
+	log.Infof("Starting service ...")
 	http.HandleFunc("/", mainHandler)
 	http.HandleFunc("/admin/health", healthCheckHandler)
 	http.Handle("/admin/metrics", promhttp.Handler())
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Println("... shutting down")
+	if err := http.ListenAndServe(":"+strconv.Itoa(port), nil); err != nil {
+		log.Infoln("... shutting down")
 	}
 }
 
 func mainHandler(w http.ResponseWriter, r *http.Request) {
+	log.WithFields(log.Fields{"path": r.URL.Path, "host": r.URL.Hostname()}).Info()
+
 	w.WriteHeader(http.StatusOK)
 	io.WriteString(w, `implement me`)
 }
