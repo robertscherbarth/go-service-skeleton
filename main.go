3253f6e2f795490c095f6170c4e9da427e4a0754
Robert Scherbarth <r.scherbarth@icloud.com>
2020-03-13 22:51:27 +0100

    add envconfig to utilize environment vars

--- main.go
+++ main.go
@@ -1,39 +1,62 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-chi/chi"
 	"io"
 	"net/http"
+	"os"
 
+	"github.com/kelseyhightower/envconfig"
 	log "github.com/sirupsen/logrus"
 
 	"github.com/robertscherbarth/go-service-skeleton/pkg/app"
 	requestLogger "github.com/robertscherbarth/go-service-skeleton/pkg/log"
 )
 
-const port = 8080
+type ServiceConfig struct {
+	Port      int    `envconfig:"port" default:"8080"`
+	LogFormat string `default:"text"`
+}
 
 func main() {
-	logger := log.New()
-	logger.Formatter = &log.TextFormatter{
-		DisableTimestamp: true,
+	var serviceConfig ServiceConfig
+	err := envconfig.Process("service", &serviceConfig)
+	if err != nil {
+		fmt.Printf("can't map env to config with err: %v\n", err)
+		os.Exit(1)
 	}
 
+	logger := createLogger(serviceConfig.LogFormat)
 	logger.Infof("starting service ...")
-	runningService(logger)
+	runningService(logger, serviceConfig)
 
 }
 
-func runningService(logger *log.Logger) {
+func runningService(logger *log.Logger, config ServiceConfig) {
 	router := chi.NewRouter()
 
-	//define additional routes
+	//TODO: define additional routes
 	router.HandleFunc("/", mainHandler)
 
-	app := app.NewApp(&app.Config{HTTPListenPort: port}, router, logger)
+	app := app.NewApp(&app.Config{HTTPListenPort: config.Port}, router, logger)
 	app.CreateRouteConfiguration(requestLogger.NewStructuredLogger(logger))
 
 	app.Start()
+}
+
+func createLogger(logFormat string) *log.Logger {
+	logger := log.New()
+	if logFormat == "text" {
+		logger.Formatter = &log.TextFormatter{
+			DisableTimestamp: true,
+		}
+		return logger
+	}
+	logger.Formatter = &log.JSONFormatter{
+		DisableTimestamp: true,
+	}
+	return logger
 }
 
 func mainHandler(w http.ResponseWriter, r *http.Request) {
